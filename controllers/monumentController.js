29ea892bfd25bc902322aa2b30785141f7bd02ac
Robell <robellasfaw@gmail.com>
2025-04-01 14:05:33 +0100

    Add GET endpoint for category

--- controllers/monumentController.js
+++ controllers/monumentController.js
@@ -17,6 +17,20 @@ const getAllMonuments = async (_req, res) => {
   }
 };
 
+const getAllCategories = async (_req, res) => {
+  const sql = "SELECT DISTINCT category FROM monuments";
+
+  try {
+    const [results] = await connection.query(sql);
+
+    if (!results.length) {
+      res.status(400).json({ message: "No category listed in DB" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 const addMonument = async (req, res) => {
   const formData = req.body;
   const sql = "INSERT INTO monuments SET ?";
@@ -38,4 +52,4 @@ const addMonument = async (req, res) => {
   }
 };
 
-export { getAllMonuments, addMonument };
+export { getAllMonuments, addMonument, getAllCategories };
--- routes/monumentRoutes.js
+++ routes/monumentRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import {
   addMonument,
+  getAllCategories,
   getAllMonuments,
 } from "../controllers/monumentController.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllMonuments).post(addMonument);
+
+router.route("/categories").get(getAllCategories);
 
 export default router;
