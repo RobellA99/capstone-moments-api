4eedf1a36478a2d2a8476f260c55b15b1cf8314f
Robell <robellasfaw@gmail.com>
2025-04-09 16:59:29 +0100

    Add drop and create commands

--- db/moments.sql
+++ db/moments.sql
@@ -1,3 +1,9 @@
+DROP DATABASE IF EXISTS `moments`;
+
+CREATE DATABASE `moments`;
+
+USE `moments`;
+
 CREATE TABLE `monuments` (
     `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     `name` varchar(255) NOT NULL, 
